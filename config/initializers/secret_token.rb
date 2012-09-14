37dba2c0cff7b16681de98c634f0f641518cac3a
Igor Kapkov <igasgeek@me.com>
2012-09-15 01:09:04 +0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twijector::Application.config.secret_token = 'dc9aa201427cb5a70db7db0cee30c8a7cabb3fe3a22e3a178433cd3c94401e5d4967c90580cc5f48d9450e116c52c95c655839523010962f5edf81d9a3663c91'
